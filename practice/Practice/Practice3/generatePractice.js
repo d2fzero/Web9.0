8040f46e1fe43ce6a1d495de4cabd8350549f7c5
vu long <vuduylong89@gmail.com>
2017-10-07 12:07:01 +0700

    hw 1

--- practice/Practice/Practice3/generatePractice.js
+++ practice/Practice/Practice3/generatePractice.js
@@ -3,15 +3,15 @@
 // function generate(numberOfTestcases, filePath = "./test-data.json"){
 //   return Array.from(Array(numberOfTestcases)); // Remove this line and change to your own algorithm
 // }
-function randomInt(min, max) {
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function generateInput() {
   var input = [];
-  var length = randomNum(0, 500);
+  var length = randomNumber(0, 500);
   for (var i = 0; i < length; i++) {
-    input[i] = randomNum(-10000, 10000);
+    input[i] = randomNumber(-10000, 10000);
   }
   input.sort((a, b) => a - b);
   return input;
@@ -27,7 +27,7 @@ function generateInput() {
         arr.push(obj);
 
         //Not found
-        let input = generateInputArray();
+        let input = generateInput();
         let temp = randomInt(-10000, 10000);
         while (input.indexOf(temp) !== -1)
         {
@@ -41,7 +41,7 @@ function generateInput() {
         arr.push(obj);
 
         //First index
-        input = generateInputArray();
+        input = generateInput();
         obj = {
             "input": input,
             "target": input[0],
@@ -50,7 +50,7 @@ function generateInput() {
         arr.push(obj);
 
         //Last index
-        input = generateInputArray();
+        input = generateInput();
         obj = {
             "input": input,
             "target": input[input.length - 1],
@@ -59,7 +59,7 @@ function generateInput() {
         arr.push(obj);
 
         //Middle index
-        input = generateInputArray();
+        input = generateInput();
         obj = {
             "input": input,
             "target": input[Math.floor(input.length / 2)],
@@ -68,7 +68,7 @@ function generateInput() {
         arr.push(obj);
 
         for (let i = 5; i < numberOfTestcases; i++) {
-            input = generateInputArray();
+            input = generateInput();
             temp = randomInt(0, input.length);
             obj = {
                 "input": input,
@@ -82,7 +82,6 @@ function generateInput() {
     writeFile(filePath, arr);
     return arr;
 
-//    return Array.from(Array(numberOfTestcases)); // Remove this line and change to your own algorithm
 }
 
 module.exports = generate
